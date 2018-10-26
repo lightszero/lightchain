a256416e882704cb1a81951f50dbca27d8077eeb
lights <lightsever@hotmail.com>
2018-10-26 22:07:40 +0800

    begin websocket thing

---
+++ lightchain.db/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+namespace lightchain.db
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var version = Assembly.GetEntryAssembly().GetName().Version;
+            Console.WriteLine("lightchain " +version);
+            while (true)
+            {
+                Console.Write("-->");
+                Console.ReadLine();
+            }
+        }
+    }
+}
